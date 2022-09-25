e56cfde26b444cddeb41574dc7cd93b73ec2e312
Christofer Bond <chrisbond66@gmail.com>
2022-09-24 19:23:55 -0700

    added psw verification

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -47,11 +47,32 @@ router.post("/", (req, res) => {
     });
 });
 
+router.post("/login", (req, res) => {
+  // expects {email, password}
+  User.findOne({
+    where: {
+      email: req.body.email,
+    },
+  }).then((dbUserData) => {
+    if (!dbUserData) {
+      res.status(400).json({ message: "No user with that email address!" });
+      return;
+    }
+    const validPassword = dbUserData.checkPassord(req.body.password);
+    if (!validPassword) {
+      res.status(400).json({ message: "Incorrect password! " });
+      return;
+    }
+    res.json({ user: dbUserData, message: "You are now logged in! " });
+  });
+});
+
 router.put("/:id", (req, res) => {
   // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
 
   // pass in req.body instead to only update what's passed through
   User.update(req.body, {
+    individualHooks: true,
     where: {
       id: req.params.id,
     },
